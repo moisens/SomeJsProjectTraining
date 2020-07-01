75ec223483b001828443143f120aea54b310f7f9
moise <mose.ns@hotmail.com>
2020-07-01 22:50:38 +0200

    Counter

---
+++ counter/app.js
@@ -0,0 +1,33 @@
+let counter = document.querySelector('.counter');
+const addCount = document.querySelector('#addCountBtn');
+const lowerCount = document.querySelector('#lowerCountBtn');
+let count = 0;
+
+addCount.addEventListener('click', incrementCounter);
+lowerCount.addEventListener('click', decrementCounter);
+
+
+function incrementCounter(){
+   count++;
+   counter.innerHTML = count
+   if(counter.innerHTML > '0'){
+      counter.style.color = '#3ca24d';
+   }else if(counter.innerHTML === '0'){
+      counter.style.color = '#fff'
+   }
+
+   counter.animate([{opacity: '0.2'}, {opacity: '1.0'}], {duration: 100, fill: 'forwards'});
+}
+
+
+function decrementCounter(){
+   count--;
+   counter.innerHTML = count;
+   if(counter.innerHTML < '0'){
+      counter.style.color = 'red';
+   }else if(counter.innerHTML === '0'){
+      counter.style.color = '#fff'
+   }
+   counter.animate([{opacity: '0.2'}, {opacity: '1.0'}], {duration: 100, fill: 'forwards'});
+}
+
